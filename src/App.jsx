54d7e11a38ac8a85b1d13d961ad77bd6b33a9d1a
devserao <seraotourjo@gmail.com>
2026-09-12 13:50:07 +0600

    added header and Hero section with mobile responsive

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Banner></Banner>
+    </>
+  );
+}
+
+export default App;
