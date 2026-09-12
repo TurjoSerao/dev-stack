b9d64b53c686f4d635dd607d98a9621b699a8831
devserao <seraotourjo@gmail.com>
2026-09-12 15:01:39 +0600

    fetching data and showing it on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { Suspense } from "react";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Technologies from "./components/technologies/Technologies";
+
+const technologiesData = async () => {
+  const res = await fetch("/data.json");
+  const data = await res.json();
+  return data;
+};
 
 function App() {
+  const technologiesPromise = technologiesData();
+  console.log(technologiesPromise);
   return (
     <>
       <Header></Header>
       <Banner></Banner>
-
+      <Suspense fallback={<h2>Loading....</h2>}>
+        <Technologies technologiesPromise={technologiesPromise}></Technologies>
+      </Suspense>
       <Footer></Footer>
     </>
   );
